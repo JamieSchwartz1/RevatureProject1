4c277dbbd70b0fe85609dffcb96471240f621c37
JamieSchwartz1 <96748302+JamieSchwartz1@users.noreply.github.com>
2022-02-17 08:01:42 -0600

    Add files via upload

---
+++ P0/GetProduct.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P0
+{//credit Miles Lenane
+    public class GetProduct
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public decimal ProductPrice { get; set; }
+        public string ProductDesc { get; set; }
+    }
+}
